2b44e8538bd4c8320730a439ccb70ad8450ff95b
2sk96 <2sk96@naver.com>
2025-04-17 22:45:16 +0900

    new consumable/equipment/crafting tab inventory using toggle group/toggle

---
+++ PROJECT_SL/Assets/01_PROJECT_SL/Scripts/UI/Inventory/ToggleControllerUI.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProjectSL
+{
+    
+    
+    public class ToggleControllerUI : MonoBehaviour
+    {
+        private enum TabContentType
+        {
+            Consumable,
+            Equipment,
+            Crafting,
+        };
+
+        [SerializeField] private Toggle consumableTab;
+        [SerializeField] private Toggle equipmentTab;
+        [SerializeField] private Toggle craftingTab;
+
+        [SerializeField] private GameObject consumableContent;
+        [SerializeField] private GameObject equipmentContent;
+        [SerializeField] private GameObject craftingContent;
+
+        private TabContentType currentActiveTabContent = TabContentType.Consumable;
+
+        private void Awake()
+        {
+            consumableTab.onValueChanged.AddListener((isOn) => {
+                consumableContent.SetActive(isOn);
+                if (isOn) currentActiveTabContent = TabContentType.Consumable;
+                });
+            equipmentTab.onValueChanged.AddListener((isOn) => { 
+                equipmentContent.SetActive(isOn);
+                if (isOn) currentActiveTabContent = TabContentType.Equipment;
+            });
+            craftingTab.onValueChanged.AddListener((isOn) => { 
+                craftingContent.SetActive(isOn);
+                if (isOn) currentActiveTabContent = TabContentType.Crafting;
+            });
+        }
+
+        private void OnEnable()
+        {
+            currentActiveTabContent = TabContentType.Consumable;
+            
+            consumableTab.isOn = true;
+            equipmentTab.isOn = false;
+            craftingTab.isOn = false;
+
+            consumableContent.SetActive(true);
+            equipmentContent.SetActive(false);
+            craftingContent.SetActive(false);
+        }
+
+        // 오른쪽 화살표 클릭
+        public void OnClickNextTab()
+        {
+            switch (currentActiveTabContent)
+            {
+                case TabContentType.Consumable: equipmentTab.isOn = true; break;
+                case TabContentType.Equipment: craftingTab.isOn = true; break;
+                case TabContentType.Crafting: consumableTab.isOn = true; break;
+            }
+        }
+
+        // 왼쪽 화살표 클릭
+        public void OnClickPrevTab()
+        {
+            switch (currentActiveTabContent)
+            {
+                case TabContentType.Consumable: craftingTab.isOn = true; break;
+                case TabContentType.Equipment: consumableTab.isOn = true; break;
+                case TabContentType.Crafting: equipmentTab.isOn = true; break;
+            }
+        }
+    }
+}
